5f8e28dac70884ff71bc1c6ac98d6978de072eff
Kevin Huber <kevin.huber@hotmail.com>
2018-12-12 11:13:13 +0100

    Added extensions

---
+++ PCPKotlinTopics/src/ch/hslu/pcp/kotlin/extensions/Extensions.kt
@@ -0,0 +1,32 @@
+package ch.hslu.pcp.kotlin.extensions
+
+import java.util.stream.Collectors
+
+fun String.capitalizeEachWord(): String {
+    return this.split(" ").stream().map { t -> t.capitalize() }.collect(Collectors.joining(" "))
+}
+
+fun Int.square(): Int {
+    return this*this
+}
+
+fun Any?.toString(): String {
+    if (this == null) return "null"
+    return toString()
+}
+
+fun main(args: Array<String>) {
+    val testString = "This is a string"
+    println(testString)
+    println(testString.capitalizeEachWord())
+
+    val testInt = 12
+    println(testInt)
+    println(testInt.square())
+
+    var nullableVariable : Any? = null
+    println(nullableVariable.toString())
+
+    nullableVariable = "This string is not null"
+    println(nullableVariable.toString())
+}
